baa9ebdaf26f2fdfddc3e6cbd0f516ac85b1cf8a
Tom Schmiedlechner <TSchmiedlechner@users.noreply.github.com>
2020-08-13 21:05:20 +0200

    Update PosClient.java

--- src/main/java/eu/fiskaltrust/middleware/demo/grpc/PosClient.java
+++ src/main/java/eu/fiskaltrust/middleware/demo/grpc/PosClient.java
@@ -54,20 +54,4 @@ public class PosClient {
         IPOS.ReceiptResponse response = blockingStub.sign(receiptRequest);
         return response;
     }
-
-
-//    public void sign(String receiptRequestJson) {
-//        logger.info("Receipt request: ");
-//        var request = IPOS.ReceiptRequest.newBuilder();
-//        request.
-//        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
-//        HelloReply response;
-//        try {
-//            response = blockingStub.sayHello(request);
-//        } catch (StatusRuntimeException e) {
-//            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-//            return;
-//        }
-//        logger.info("Greeting: " + response.getMessage());
-//    }
 }
